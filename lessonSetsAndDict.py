925cb44eae38fd0240a232fe1de015f0336feb66
Jonathan Kelly <jdk5115@gmail.com>
2020-07-11 14:41:36 -0400

    Create lessonSetsAndDict.py

---
+++ lessonSetsAndDict.py
@@ -0,0 +1,25 @@
+#sets and dictionaries
+#sets don't keep position and will not repeat when printed
+#good to check if something is there, not where it is
+
+# sets = {"element0","element2","element5.6","element4"}
+# print(sets)
+# if "element1" in sets:
+#     print("yeah")
+
+# countrylist = []
+# for i in range(5):
+#     country = input("what is your country: ")
+#     countrylist.append(country)
+
+
+# countryset = set(countrylist)
+# print(countryset)
+# print(countrylist)
+
+# if "us" in countryset:
+#     print("attended")
+
+#Dictionaries:
+#Dictionaries have a key and a value pair
+#Dictionary = {}
