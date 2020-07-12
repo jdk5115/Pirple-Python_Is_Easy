ac62805dec015d29b9f4fcbabad830d00923343b
Jonathan Kelly <jdk5115@gmail.com>
2020-07-11 20:48:11 -0400

    update

---
+++ Homework7setsAndDict.py
@@ -0,0 +1,42 @@
+'''
+Homework #7 copied from Homework #1 pirple
+Sets and dictionaries
+'''
+
+#Artist and Genre info
+favSong = {
+    "Artist":"Marshmello",   
+    "Song":"Blocks",
+    "DurationInMinutes":3.483,
+    "Producer":"Wild Bill",
+    "Label":"House label",
+    "NumOfSpotifyListens":1400000,
+    "YearRecorded":2016,
+    "AlbumName":"Joytime",
+    "AlbumArtwork":"Some Lady",
+    "SalesinUSDollars":4350000.27
+    }
+
+while(True):
+    key = int(input("What item do you want to guess?"))
+    value = int(input("what is your guess?"))
+
+    def guessFunc(key,value):
+        if key in favSong:
+            if value in favSong:
+                print("hey now")
+            else:
+                print("nah")
+
+    guessFunc(key,value)
+
+# print(Artist)
+# print(Song)
+# print(DurationInMinutes)
+# print(Producer)
+# print(Label)
+# print(NumOfSpotifyListens)
+# print(YearRecorded)
+# print(AlbumName)
+# print(AlbumArtwork)
+# print(SalesinUSDollars)
--- dictionaryAndSetsExamples.py
+++ dictionaryAndSetsExamples.py
@@ -3,4 +3,14 @@
 blackshoes = {42:2, 41:3, 40:4, 39:1, 38:0}
 
 while (True):
-    purchaseSize = input("What size do you want to buy?\n")
+    purchaseSize = int(input("What size do you want to buy?\n"))
+    if purchaseSize < 0:
+        break
+    if purchaseSize in blackshoes:
+        if blackshoes[purchaseSize] > 0:
+            blackshoes[purchaseSize] -=1
+            print(blackshoes)
+        else:
+            print("We do not have that size in stock, sorry :(")
+    else:
+        print("don't have that size sorry")
