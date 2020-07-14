6ac9ca9211950e05aa7ac6993e7defbab7029839
Jonathan Kelly <jdk5115@gmail.com>
2020-07-14 19:41:31 -0400

    update 2

---
+++ fileI-O.py
@@ -0,0 +1,20 @@
+#file input output pirple
+# "r" - read, "w" - write, "a" - append, "r+"" - read and write
+# file = open("filename","r",)
+# file.close()
+
+cities = ["London", "Paris", "Oshawa", "Tokyo", "LA"]
+
+cityfile = open("cityplaces", "w", )
+
+for spots in cities:
+    cityfile.write(spots) #needs to be a string
+    
+cityfile.close()
+
+cityfile = open("cityplaces", "r")
+
+wholefile = cityfile.read()
+print(wholefile)
+print("done")
+wholefile.close()
