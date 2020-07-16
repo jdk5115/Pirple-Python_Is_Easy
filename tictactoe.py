b68dfda5ced27eae3e5a41367f4597387fc22ef6
Jonathan Kelly <jdk5115@gmail.com>
2020-07-16 17:44:38 -0400

    tic tac toe update 2

--- tictactoe.py
+++ tictactoe.py
@@ -24,13 +24,16 @@ def drawfield():
 
 player = 1
 #[Column1 [row1, row 2, row3}, Column2[Row 1, row 2, row 3]...]]
-currentfiled = [[" ", " ", " "]], [" ", " ", " "], [" ", " ", " "]]
+currentfield = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
 while(True):
     moveRow = int(input("Please enter the row."))
     moveColumn = int(input("Please enter the column."))
     if player == 1:
         # make move for player 1
-        currentfield[moveColumn]]
+        currentfield[moveColumn][moveRow] = "X"
+        player = 2
     else:
         # make move for player two
+        currentfield[moveColumn][moveRow] = "O"
+        player = 1
