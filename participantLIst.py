bf3a90c2f7061b5d5eb5cdd058a7730303630a89
Jonathan Kelly <jdk5115@gmail.com>
2020-07-17 15:50:50 -0400

    participant data update

--- participantLIst.py
+++ participantLIst.py
@@ -1,9 +1,9 @@
 #pirple I/O participant data
 
-participantNumber = 5
+participantNumber = 2
 participantData = []
-
 registeredparticipants = 0
+outputFile = open("participantData.txt","w")
 
 while(registeredparticipants < participantNumber):
     tempPartData = []
@@ -11,7 +11,19 @@ while(registeredparticipants < participantNumber):
     tempPartData.append(name)
     country = input("Please enter your country of origin: ")
     tempPartData.append(country)
-    age = int(input("Please enter your age: ")
+    age = int(input("Please enter your age: "))
     tempPartData.append(age)
-
+    print(tempPartData)
     participantData.append(tempPartData)
+    print(participantData)
+
+    registeredparticipants += 1
+
+for participant in participantData:
+    #the line below is to loop through the nested list
+    for data in participant:
+        outputFile.write(str(data))
+        outputFile.write(" ")
+    outputFile.write(" \n")
+
+outputFile.close()
