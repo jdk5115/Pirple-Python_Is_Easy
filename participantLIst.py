e4701945fc4fa0eb129a11a64f2511aca71c271d
Jonathan Kelly <jdk5115@gmail.com>
2020-07-17 14:51:00 -0400

    Create participantLIst.py

---
+++ participantLIst.py
@@ -0,0 +1,17 @@
+#pirple I/O participant data
+
+participantNumber = 5
+participantData = []
+
+registeredparticipants = 0
+
+while(registeredparticipants < participantNumber):
+    tempPartData = []
+    name = input("Please enter your name: ")
+    tempPartData.append(name)
+    country = input("Please enter your country of origin: ")
+    tempPartData.append(country)
+    age = int(input("Please enter your age: ")
+    tempPartData.append(age)
+
+    participantData.append(tempPartData)
