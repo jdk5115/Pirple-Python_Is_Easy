d15fa024e816c19e1316295ac3d6d766ca86ffe5
Jonathan Kelly <jdk5115@gmail.com>
2020-07-21 13:06:38 -0400

    Update participantLIst.py

--- participantLIst.py
+++ participantLIst.py
@@ -1,32 +1,32 @@
 #pirple I/O participant data
 
-participantNumber = 5
-participantData = []
-registeredparticipants = 0
-outputFile = open("participantData.txt","w")
+# participantNumber = 5
+# participantData = []
+# registeredparticipants = 0
+# outputFile = open("participantData.txt","w")
 
-while(registeredparticipants < participantNumber):
-    tempPartData = []
-    name = input("Please enter your name: ")
-    tempPartData.append(name)
-    country = input("Please enter your country of origin: ")
-    tempPartData.append(country)
-    age = int(input("Please enter your age: "))
-    tempPartData.append(age)
-    print(tempPartData)
-    participantData.append(tempPartData)
-    print(participantData)
+# while(registeredparticipants < participantNumber):
+#     tempPartData = []
+#     name = input("Please enter your name: ")
+#     tempPartData.append(name)
+#     country = input("Please enter your country of origin: ")
+#     tempPartData.append(country)
+#     age = int(input("Please enter your age: "))
+#     tempPartData.append(age)
+#     print(tempPartData)
+#     participantData.append(tempPartData)
+#     print(participantData)
 
-    registeredparticipants += 1
+#     registeredparticipants += 1
 
-for participant in participantData:
-    #the line below is to loop through the nested list
-    for data in participant:
-        outputFile.write(str(data))
-        outputFile.write(" ")
-    outputFile.write(" \n")
+# for participant in participantData:
+#     #the line below is to loop through the nested list
+#     for data in participant:
+#         outputFile.write(str(data))
+#         outputFile.write(" ")
+#     outputFile.write(" \n")
 
-outputFile.close()
+# outputFile.close()
 
 inputFile = open("participantData.txt","r")
 
@@ -50,6 +50,10 @@ for part in inputList:
 print(Age)
 
 oldest = 0
+for tempAge in Age:
+    if tempAge > oldest:
+        oldest = tempAge
+print(oldest)
 
 
 inputFile.close()
