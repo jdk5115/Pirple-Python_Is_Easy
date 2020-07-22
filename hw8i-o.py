05c9a3e068056e4f8f0332adb66e1075718e2c67
Jonathan Kelly <jdk5115@gmail.com>
2020-07-21 20:54:03 -0400

    Create hw8i-o.py

---
+++ hw8i-o.py
@@ -0,0 +1,50 @@
+# input output homework pirple
+
+''' 
+Details:
+Create a note-taking program. When a user starts it up, it should prompt them for a filename.
+If they enter a file name that doesn't exist, it should prompt them to enter the text they want to write to the file. After they enter the text, it should save the file and exit.
+If they enter a file name that already exists, it should ask the user if they want:
+    A) Read the file
+    B) Delete the file and start over
+    C) Append the file
+If the user wants to read the file it should simply show the contents of the file on the screen. If the user wants to start over then the file should be deleted and another empty one made in its place. If a user elects to append the file, then they should be able to enter more text, and that text should be added to the existing text in the file. 
+
+Extra Credit:
+Allow the user to select a 4th option:
+    D) Replace a single line
+
+If the user wants to replace a single line in the file, they will then need to be prompted for 2 bits of information:
+    1) The line number they want to update.
+    2) The text that should replace that line. 
+'''
+
+fileName = input("What do you want the filename to be?") + ".txt"
+
+#if fileName Exists
+choice = input("Do you want to: \n A) Read the File \n B) Delete the file and start over \n C) Append the file \n ")
+
+    if choice == "A":
+        # open(fileName,"r")
+        # fileName.readlines
+    elif choice == "B":
+        # filename.delete??
+        # createFile = open(fileName,"w")
+    elif choice == "C":
+        fileAppend = input("What would you like to add to the file?")
+        #fileName.append(fileAppend)
+    else:
+        print("Please only choose either A, B, or C.")
+#else:
+    # data = input("What do you want to input into file?")
+    # open(fileName,"w")
+    # filename.write(data)
+    # filename.close()
+
+
+createFile.close()
+
+
+
+
+print(fileName)
