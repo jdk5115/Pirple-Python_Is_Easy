1ae26c5cbcf2ce3c2a2245870f7c322a7f47f85d
Jonathan Kelly <jdk5115@gmail.com>
2020-09-03 21:52:38 -0400

    Create pirpleClasses.py

---
+++ pirpleClasses.py
@@ -0,0 +1,12 @@
+# Pirple Classes - Section 9
+
+# class ClassName: 
+
+#     def __init__(self):
+#         self.Attribute = 0
+
+#     def AnotherFuntion(self):
+#         Action(s)
+
+class Team:
+    def __in__(self):)
