7321726438e17d6dd58a4f4b83b1987de2131fe6
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 15:46:03 -0400

    Update ImportGuessingGame.py

--- ImportGuessingGame.py
+++ ImportGuessingGame.py
@@ -4,3 +4,14 @@ from random import randint
 
 randVal = randint(0,100)
 
+while(True):
+    guess = int(input("What is your guess? "))
+    if randVal == guess:
+        break
+    elif guess < randVal:
+        print("Your guess was too low!")
+    else:
+        print("Your guess is too high!")
+
+print("You've won!!! The correct guess was:", guess)
+
