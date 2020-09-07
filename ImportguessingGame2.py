45e201ad3e55e39a3405e0bd831d44cc7432bfad
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 16:09:57 -0400

    Update ImportguessingGame2.py

--- ImportguessingGame2.py
+++ ImportguessingGame2.py
@@ -1,14 +1,15 @@
 #Import Guessing game #2
-
+from time import perf_counter
 from random import random
 
 randVal = random() #random float 0.0 < N < 1.0
-
 upper = 1.0
 lower = 0.0
 guess = 0.5
+startTime = perf_counter()
 
 while(True):
+    guess = (upper + lower) / 2
     if guess == randVal:
         break
     elif guess < randVal:
@@ -19,9 +20,11 @@ while(True):
         upper = guess
         print(guess)
         print("Too High!")
-    guess = (upper + lower) / 2
+
+endTime = perf_counter()
 
 print("Nailed it! The number was:", guess)
+print("That took", endTime - startTime ,"seconds.")
 
 
 
