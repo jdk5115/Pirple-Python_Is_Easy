ec2a8b26813869560394befefe441134fbf2197b
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 14:30:02 -0400

    Update ImportingLesson.py

--- ImportingLesson.py
+++ ImportingLesson.py
@@ -2,8 +2,16 @@
 
 import random
 
-random.seed(99)
+#random.seed(1)
 
 randInt = random.randint(0,100) #random from start <= N <= end. Includes 0 and 100
-
 print(randInt)
+
+randFloat = random.random() #0.0 <= N <= 1.0
+print(randFloat)
+
+trueRand = randFloat*randInt
+print(trueRand)
+
+randUniform = random.uniform(1,11) # start <= N <= end
+print(randUniform)
