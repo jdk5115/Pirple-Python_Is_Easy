d388d1ef7e91aa0d14a1b9eebe9fbb5ea66f2f55
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 12:17:23 -0400

    Update classesHomework.py

--- classesHomework.py
+++ classesHomework.py
@@ -9,8 +9,8 @@ xIt should have another method called "Stop" that sets the value of isDriving to
 xSwitching isDriving from true to false should increment the "TripsSinceMaintenance" counter. 
 xAnd when TripsSinceMaintenance exceeds 100, then the NeedsMaintenance boolean should be set to true.
 xAdd a "Repair" method to either class that resets the TripsSinceMaintenance to zero, and NeedsMaintenance to false.
-Create 3 different cars, using your Cars class, and drive them all a different number of times. 
-Then print out their values for Make, Model, Year, Weight, NeedsMaintenance, and TripsSinceMaintenance
+xCreate 3 different cars, using your Cars class, and drive them all a different number of times. 
+xThen print out their values for Make, Model, Year, Weight, NeedsMaintenance, and TripsSinceMaintenance
 
 Extra Credit:
 Create a Planes class that is also an inheritance class from Vehicle. Add methods to the Planes class for Flying and Landing (similar to Driving and Stopping), 
@@ -45,11 +45,10 @@ class Vehicle:
         self.tripsSinceMaintenance = x
 
 class Cars(Vehicle):
-    def __init__(self, isDriving, make, model, year, weight, needsMaintenance, tripsSinceMaintenance):
+    def __init__(self, isDriving, make, model, year, weight, needsMaintenance = False, tripsSinceMaintenance = 0):
         Vehicle.__init__(self, make, model, year, weight, needsMaintenance, tripsSinceMaintenance)
         self.isDriving = isDriving
 
-    
     def Drive(self):
         self.isDriving = True
 
@@ -62,3 +61,29 @@ class Cars(Vehicle):
     def Repair(self):
         self.tripsSinceMaintenance = 0
         self.needsMaintenance = False
+    
+    def __str__(self):
+        return "Make and model " + str(self.make) + " - " + str(self.model) + "\nYear: " + str(self.year) + "\nWeight: " + str(self.weight) + "\nDoes this vehicle need maintenance? " + str(self.needsMaintenance) + " \nHow many trips since the last maintenance? " + str(self.tripsSinceMaintenance) + "\n\n"
+
+car1 = Cars(False,"Chevy", "Corvette", "2005", 1985)
+car2 = Cars(False, "Ford", "F-150", "2018", 2989)
+car3 = Cars(False, "Tesla", "X P100D", "2020", 1587)
+
+
+for x in range (150):
+    Cars.Drive(car1)
+    Cars.Stop(car1)
+
+print(str(car1))
+
+for x in range (84):
+    Cars.Drive(car2)
+    Cars.Stop(car2)
+
+print(str(car2))
+
+for x in range (240):
+    Cars.Drive(car3)
+    Cars.Stop(car3)
+
+print(str(car3))
