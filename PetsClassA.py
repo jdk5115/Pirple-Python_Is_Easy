c1de7f1759e9069a4024b559c3b38a1d85eff0d0
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 08:03:58 -0400

    Update PetsClassA.py

--- PetsClassA.py
+++ PetsClassA.py
@@ -6,3 +6,29 @@ class Pets:
         self.age = a
         self.hunger = h
         self.playful = p
+
+    #getters
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+    def getHunger(self):
+        return self.hunger
+
+    def getPlayful(self):
+        return self.playful   
+
+    #setters
+    def setName(self, x):
+        self.name = x
+
+    def setAge(self, Age):
+        self.age = Age
+
+    def setHunger(self, hunger):
+        self.hunger = hunger
+
+    def setPlayful(self, playful):
+        self.playful = playful
