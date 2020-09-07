e14726658880d6b5d6506bf89dca8b13b2d63953
Jonathan Kelly <jdk5115@gmail.com>
2020-09-07 09:13:07 -0400

    name change

--- PetsClassLessons.py
+++ PetsClassLessons.py
@@ -1,4 +1,4 @@
-# Pets Class Lesson A
+'''#Classes - Pets: Lesson A'''
 
 class Pets:
     def __init__(self, n, a, h, p):
@@ -32,3 +32,18 @@ class Pets:
 
     def setPlayful(self, playful):
         self.playful = playful
+    
+    def __str__(self):
+        return self.name + str(self.playful) + str(self.hunger) + str(self.age)
+
+
+Pet1 = Pets("Fido", 43, False, True)
+
+print(Pet1.getName())
+print(Pet1.getPlayful())
+Pet1.setName("Snowball")
+print(Pet1.getName())
+print(Pet1.name)
+Pet1.name = "Jim"
+print(Pet1.name)
+print(Pet1)
