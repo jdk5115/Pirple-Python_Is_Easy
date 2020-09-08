d5b98ee728267683fc5394528c4fc856a969161f
Jonathan Kelly <jdk5115@gmail.com>
2020-09-08 14:58:34 -0400

    Update ImportingHomework2.py

--- ImportingHomework2.py
+++ ImportingHomework2.py
@@ -6,7 +6,7 @@ window = tk.Tk()
 window.title("my box gui")
 window.iconbitmap('target_goal_icon_152113.ico')
 window.geometry("500x500") #width x height
-global lbl1
+
 lbl1 = tk.Label(window, text='Hello! Welcome to the program.  What should I call you?')
 lbl1.pack(pady=10)
 
@@ -21,6 +21,8 @@ def func1():
     lbl1 = tk.Label(window, text='Welcome to the program ' + e.get())
     lbl1.pack(pady=20)
     e.delete(0, 'end')
+    e.destroy()
+    btn1.pack_forget()
 
 btn1 = tk.Button(window, text="submit", command= lbl1Erase, fg= 'black', bg='#33FFC4', padx=40, pady=20)
 btn1.pack()
