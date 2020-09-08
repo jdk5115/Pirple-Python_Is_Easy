bddade234d544004edd324a5d4ae2a8fa77e87bc
Jonathan Kelly <jdk5115@gmail.com>
2020-09-08 14:35:34 -0400

    Update ImportingHomework2.py

--- ImportingHomework2.py
+++ ImportingHomework2.py
@@ -6,17 +6,24 @@ window = tk.Tk()
 window.title("my box gui")
 window.iconbitmap('target_goal_icon_152113.ico')
 window.geometry("500x500") #width x height
-
-lbl1 = tk.Label(window, text='Hello! Welcome to the program.  What should I call you?').grid(row=0,column=1)
+global lbl1
+lbl1 = tk.Label(window, text='Hello! Welcome to the program.  What should I call you?')
+lbl1.pack(pady=10)
 
 e = tk.Entry(window, width=50, fg='black')
-e.grid(row=5,column=1)
+e.pack(pady=10)
+
+def lbl1Erase():
+    lbl1.pack_forget()
+    func1()
 
 def func1():
-    lbl1 = tk.Label(window, text='Welcome to the program ' + e.get() ).grid(row=0,column=1)
+    lbl1 = tk.Label(window, text='Welcome to the program ' + e.get())
+    lbl1.pack(pady=20)
+    e.delete(0, 'end')
 
-btn1 = tk.Button(window, text="submit", command= func1, fg= 'black', bg='#33FFC4', padx=40, pady=20).grid(row=20,column=1)
-
+btn1 = tk.Button(window, text="submit", command= lbl1Erase, fg= 'black', bg='#33FFC4', padx=40, pady=20)
+btn1.pack()
 
 
 window.mainloop()
