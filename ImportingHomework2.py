ed840bb214a28fb1516ab7a6424d2802313dad6a
Jonathan Kelly <jdk5115@gmail.com>
2020-09-08 13:31:40 -0400

    tkinter

--- ImportingHomework2.py
+++ ImportingHomework2.py
@@ -3,11 +3,12 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.title("my box gui")
+window.iconbitmap('target_goal_icon_152113.ico')
 
-window.title("my computer science glossary")
-
-lbl1 = tk.Label(window, text='yo man')
-
-lbl1.pack()
+lbl1 = tk.Label(window, text='yo man').pack()
+lbl2 = tk.Label(window, text= "Hey dude, how are you?").pack()
+    # lbl1.pack() <-- can also be done on the actual line
+    # lbl2.pack() <-- can also be done on the actual line
 
 window.mainloop()
