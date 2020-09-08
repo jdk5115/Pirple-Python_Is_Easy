d313cdd523623f44b5336e963974137b85393323
Jonathan Kelly <jdk5115@gmail.com>
2020-09-08 14:14:55 -0400

    Update ImportingHomework2.py

--- ImportingHomework2.py
+++ ImportingHomework2.py
@@ -1,14 +1,22 @@
 #tkinter
-
 import tkinter as tk
 
 window = tk.Tk()
+
 window.title("my box gui")
 window.iconbitmap('target_goal_icon_152113.ico')
+window.geometry("500x500") #width x height
 
-lbl1 = tk.Label(window, text='yo man').pack()
-lbl2 = tk.Label(window, text= "Hey dude, how are you?").pack()
-    # lbl1.pack() <-- can also be done on the actual line
-    # lbl2.pack() <-- can also be done on the actual line
+lbl1 = tk.Label(window, text='Hello! Welcome to the program.  What should I call you?').grid(row=0,column=1)
+
+e = tk.Entry(window, width=50, fg='black')
+e.grid(row=5,column=1)
+
+def func1():
+    lbl1 = tk.Label(window, text='Welcome to the program ' + e.get() ).grid(row=0,column=1)
+
+btn1 = tk.Button(window, text="submit", command= func1, fg= 'black', bg='#33FFC4', padx=40, pady=20).grid(row=20,column=1)
+
+
 
 window.mainloop()
