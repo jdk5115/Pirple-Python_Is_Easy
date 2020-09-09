133ea476e412fc1a5925b326ae3942496556aff7
Jonathan Kelly <jdk5115@gmail.com>
2020-09-09 16:18:20 -0400

    Update ImportingHomework2.py

--- ImportingHomework2.py
+++ ImportingHomework2.py
@@ -2,13 +2,13 @@
 import tkinter as tk
 import time as t
 
-global lbl1, e, slider, btn1, x, Qcount, Questions #, Bclicks, ButtonClicks, b
-
+global lbl1, e, slider, btn1, x, Qcount, Questions, window, btn1 #, Bclicks, ButtonClicks, b
+x=0
 
 window = tk.Tk()
 window.title("my box gui")
 window.iconbitmap('target_goal_icon_152113.ico')
-window.geometry("500x500") #width x height
+window.geometry("400x400") #width x height
 
 Questions = ['Hello, what should I call you1?','How old are you2?', 'What is your favorite sport?3', 
             'How many brothers and sisters do you have4?',
@@ -17,67 +17,28 @@ Questions = ['Hello, what should I call you1?','How old are you2?', 'What is you
 x = 0
 Qcount = Questions[x]
 
-b= 0
+lbl1 = tk.Label(window, bg='red',text=Qcount)
+lbl1.pack(fill=tk.X)
 
-def btnClick():
-    global lbl1, e, slider, btn1, x, Qcount, Questions, name, b
-    if b == 0:
-        name = e.get()
-        e.delete(0, 'end')
-        e.destroy()
+e = tk.Entry(window, width=50, fg='blue')
+e.pack(pady=10)
 
-    elif b == 1:
-        age = slider.get()
-        slider.destroy()
+# btn1 = tk.Button(window, text="Submit", command= btnClick() , fg= 'black', bg='#33FFC4', padx=10, pady=0)
+# btn1.pack()
 
 
-    x+=1
-    b+=1
+lbl1.pack_forget()
+lbl1 = tk.Label(window, text=Questions[x])
+lbl1.pack(pady=10)
 
+# btn1.pack_forget()
+# btn1 = tk.Button(window, text="Submit", command= btnClick() , fg= 'black', bg='#33FFC4', padx=40, pady=20)
+# btn1.pack()
 
-while(True):
-    for i in range (8):
-        if x == 0:
-            e = tk.Entry(window, width=50, fg='black')
-            e.pack(pady=10) 
+slider = tk.Scale(window,from_=0, to=100)
+slider.pack()
 
-            lbl1 = tk.Label(window, text=Qcount)
-            lbl1.pack(pady=10)
-
-            btn1 = tk.Button(window, text="Submit", command= btnClick() , fg= 'black', bg='#33FFC4', padx=40, pady=20)
-            btn1.pack()
-
-        elif x == 1:
-            lbl1.pack_forget()
-            lbl1 = tk.Label(window, text=Questions[x])
-            lbl1.pack(pady=10)
-
-            slider = tk.Scale(window,from_=0, to=100)
-            slider.pack()
-
-            btn1.pack_forget()
-            btn1 = tk.Button(window, text="Submit", command= btnClick() , fg= 'black', bg='#33FFC4', padx=40, pady=20)
-            btn1.pack()
-
-        elif x ==2:
-            
-            x+=1
-        elif x ==3:
-            
-            x+=1
-        elif x ==4:
-            
-            x+=1
-        elif x ==5:
-
-            x+=1
-        elif x ==6:
-
-            x+=1 
-        else:
-            x=0
-
-        window.mainloop()
+window.mainloop()
 
 #status = tk.Label(window, text= "Question 1" of 8")
 
@@ -127,4 +88,4 @@ while(True):
 
 
 
-window.mainloop()
+#window.mainloop()
