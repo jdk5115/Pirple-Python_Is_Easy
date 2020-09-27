9ac0d31b071847a41b1f1dbb6f750a01b10d5d6c
Jonathan Kelly <jdk5115@gmail.com>
2020-09-27 14:23:12 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -25,9 +25,116 @@ import turtle as t
 global lsWord, word, strikes
 
 strikes = 0
+#draw dashes for every letter in word
+def dashes():
+    global lsWord
+    t.penup()
+    t.setx(-350)
+    t.sety(-300)
+    for i in range(len(lsWord)):
+        t.pendown()
+        t.pensize(5) 
+        t.fd(50)
+        t.penup()
+        t.fd(30)
+        t.pendown()
+        t.penup()
+
+#draw gallows
+def gallows():
+    t.penup()
+    t.setx(250)
+    t.sety(-200)
+    t.pendown()
+    t.pensize(5)   
+    t.lt(180)
+    t.fd(100)
+    t.lt(180)
+    t.fd(200)
+    t.lt(180)
+    t.fd(100)
+    t.rt(90)
+    t.forward(300)
+    t.lt(90)
+    t.forward(200)
+    t.lt(90)
+    t.forward(50)
+
+#draw head after 1st miss
+def head():
+        t.penup()
+        t.setx(50)
+        t.sety(50)
+        t.dot(50)
+        t.penup()
+
+#draw body after 2nd miss
+def body():
+        t.penup()
+        t.setx(50)
+        t.sety(50)
+        t.down()
+        t.pendown()
+        t.pensize(5)
+        t.forward(125)
+        t.penup()
+
+#draw left leg after 3rd miss
+def leftLeg():
+    t.penup()
+    t.setx(50)
+    t.sety(-75)
+    t.down()
+    t.pendown()
+    t.rt(30)
+    t.fd(50)
+    t.penup()
+
+#draw right leg after 4th miss
+def rightLeg():
+    t.penup()
+    t.setx(50)
+    t.sety(-75)
+    t.pendown()
+    t.down()
+    t.lt(60)
+    t.fd(50)
+    t.penup()
+
+#draw left arm after 5th miss
+def leftArm():
+    t.penup()
+    t.setx(50)
+    t.sety(-25)
+    t.pendown()
+    t.down()
+    t.rt(150)    
+    t.fd(50)
+    t.penup()
+
+#draw right arm after 6th miss
+def rightArm():
+    t.penup()
+    t.setx(50)
+    t.sety(-25)
+    t.pendown()
+    t.down()
+    t.rt(120)    
+    t.fd(50)
+    t.penup()
+
+#send a nice message to the person who lost the game =)
+def youDied():
+    t.penup()
+    t.setx(-200)
+    t.sety(200)
+    t.pendown()
+    t.down()
+    t.color('red')
+    t.write('Loser', font=("Arial", 48, "normal"))
+
 
 #Allow player 1 to pick a word and then check for spaces and numbers.Save word as list - lsWord
-
 while(True):
     word = input('Player 1, please pick a word less than 8 characters long.').upper()
     if word.isalpha():
@@ -44,8 +151,11 @@ while(True):
     else:
         print("Please only use letters.")
 
+dashes()
+gallows()
+
 while(strikes < 6):
-    guess = input('Please make your guess.').upper()
+    guess = input('Player 2, please make your guess.').upper()
     if guess.isalpha():
         if len(guess) > 1 or len(guess) < 1:
             print('Only enter 1 letter.')
@@ -70,104 +180,6 @@ func = input('>')  #raw_input on python2.x
 function_dict[func]()
 
 
-def dashes():
-    global lsWord
-    t.penup()
-    t.setx(-350)
-    t.sety(-300)
-    for i in range(len(lsWord)):
-        t.pendown()
-        t.pensize(5) 
-        t.fd(50)
-        t.penup()
-        t.fd(30)
-        t.pendown()
-        t.penup()
-
-def gallows():
-    t.penup()
-    t.setx(250)
-    t.sety(-200)
-    t.pendown()
-    t.pensize(5)   
-    t.lt(180)
-    t.fd(100)
-    t.lt(180)
-    t.fd(200)
-    t.lt(180)
-    t.fd(100)
-    t.rt(90)
-    t.forward(300)
-    t.lt(90)
-    t.forward(200)
-    t.lt(90)
-    t.forward(50)
-
-def head():
-        t.penup()
-        t.setx(50)
-        t.sety(50)
-        t.dot(50)
-        t.penup()
-
-def body():
-        t.penup()
-        t.setx(50)
-        t.sety(50)
-        t.down()
-        t.pendown()
-        t.pensize(5)
-        t.forward(125)
-        t.penup()
-
-def leftLeg():
-    t.penup()
-    t.setx(50)
-    t.sety(-75)
-    t.down()
-    t.pendown()
-    t.rt(30)
-    t.fd(50)
-    t.penup()
-
-def rightLeg():
-    t.penup()
-    t.setx(50)
-    t.sety(-75)
-    t.pendown()
-    t.down()
-    t.lt(60)
-    t.fd(50)
-    t.penup()
-
-def leftArm():
-    t.penup()
-    t.setx(50)
-    t.sety(-25)
-    t.pendown()
-    t.down()
-    t.rt(150)    
-    t.fd(50)
-    t.penup()
-
-def rightArm():
-    t.penup()
-    t.setx(50)
-    t.sety(-25)
-    t.pendown()
-    t.down()
-    t.rt(120)    
-    t.fd(50)
-    t.penup()
-
-def youDied():
-    t.penup()
-    t.setx(-200)
-    t.sety(200)
-    t.pendown()
-    t.down()
-    t.color('red')
-    t.write('Loser', font=("Arial", 48, "normal"))
 
 def letter1():
     t.penup()
