f189a626a03547fb683a5c9ca094b5399c2262d8
Jonathan Kelly <jdk5115@gmail.com>
2020-09-28 14:39:50 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -24,6 +24,7 @@ class Player:
         self.hand = hand
         self.score = self.setScore()
         self.money = money
+        self.bet = 0
 
     def __str__(self): #allows us to call print(Player)
         currentHand = "" #self.hand = ["A", "10"] -> "A 10"
@@ -38,7 +39,7 @@ class Player:
         self.score = 0
         faceCardDict = {"A":11, "K":10,"Q":10,"J":10, "10":10,"9":9,"8":8,"7":7,
                         "6":6,"5":5,"4":4,"3":3,"2":2}
-        
+
         aceCounter = 0
         for card in self.hand:
             self.score += faceCardDict[card]
@@ -47,6 +48,44 @@ class Player:
             if self.score > 21 and aceCounter != 0:
                 self.score -= 10
                 aceCounter -= 1
+        return self.score
+
+    def hit(self, card):
+        self.hand.append(card)
+        self.score = self.setScore()
+
+    def play(self, newHand):
+        self.hand = newHand
+        self.score = self.setScore()
+
+    def betMoney(self, amount):
+        self.money -= amount
+        self.bet += amount
+
+    def win(self, result):
+        if result == True: 
+            if self.score == 21 and len(self.hand) == 2:
+                self.money += int(2.5*self.bet)
+
+            else:
+                self.money += 2*self.bet
+
+            self.bet = 0
+        else:
+            self.bet = 0
 
 Player1 = Player(["3","7"])
 print(Player1)
+#Player1.hit("K")
+Player1.hit("A")
+Player1.hit("A")
+print(Player1)
+Player1.betMoney(20)
+print(Player1.money, Player1.bet)
+Player1.win(True)
+print(Player1.money, Player1.bet)
+Player1.play(["A","K"])
+print(Player1)
+Player1.betMoney(20)
+Player1.win(True)
+print(Player1.money, Player1.bet)
